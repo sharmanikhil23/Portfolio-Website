d884581d6370a3ca99d94d177e94277c922e4628
Nikhil Sharma <nikhil23062000@icloud.com>
2022-10-10 08:43:50 -0400

    made number of new updates in my portfolio website

--- src/components/firstPage/education/education.js
+++ src/components/firstPage/education/education.js
@@ -1,23 +1,25 @@
 import React from "react";
-
+import { EducationalData } from "./allEducationData";
 import "./css/education.css";
+
 export const Education = () => {
   return (
     <section id="education">
       <p id="education-heading">Education</p>
-      <div id="education-school">
-        <h1>
-          Bachelor of Computer Science Honours, Stream: Software Engineering
-        </h1>
-        <p>September 2022-September 2026</p>
-      </div>
-      <p id="education-college">Carleton University-Ottawa,Canada</p>
-      <p id="education-description">
-        Coursework: Web Programming, Java Application Programming, Web
-        Enterprise Application, Software Design and Testing, .NET Enterprise
-        Application Development, Operating Systems, Processor Architecture,
-        Compilers.
-      </p>
+
+      {EducationalData.map((value, index) => {
+        const { title, period, institution, description } = value;
+        return (
+          <div key={index}>
+            <div id="education-school">
+              <h1>{title}</h1>
+              <p>{period}</p>
+            </div>
+            <p id="education-college">{institution}</p>
+            <p id="education-description">{description}</p>
+          </div>
+        );
+      })}
       <hr></hr>
     </section>
   );
--- src/components/projects/projectsData.js
+++ src/components/projects/projectsData.js
@@ -2,44 +2,29 @@ import { BsGithub } from "react-icons/bs";
 import { RiLiveFill } from "react-icons/ri";
 
 //
-import portfolioScreenShot from "./images/portfolioScreenShot.png";
+import project1 from "./images/project1.png";
+import project2 from "./images/project2.png";
 
 const projectData = [
   {
-    image: portfolioScreenShot,
+    image: project1,
     heading: " Portfolio Website",
-    description: "Build the frountEnd Website using the React Framework ",
+    description:
+      "Build the frountEnd Website using the React Framework, html, css, scss",
     giticon: <BsGithub></BsGithub>,
     liveicon: <RiLiveFill></RiLiveFill>,
     live: "https://nikhil-sharma-portfolio.netlify.app/",
     link: "https://github.com/sharmanikhil23/Portfolio-Website",
   },
   {
-    image: portfolioScreenShot,
-    heading: " Portfolio Website",
-    description: "Build the frountEnd Website using the React Framework ",
+    image: project2,
+    heading: "To-Do Wesite",
+    description:
+      "Build the complete full stack website with the help of the MERN stack by using NodeJs, Express in the back end along with the mongoDb and on the frount end using the html, css, js, React, Scss with authentication system which will keep your notes personal. \n To test it you can use any email eg nikhil@abc.ca and any passwords 123456789",
     giticon: <BsGithub></BsGithub>,
     liveicon: <RiLiveFill></RiLiveFill>,
-    live: "https://nikhil-sharma-portfolio.netlify.app/",
-    link: "https://github.com/sharmanikhil23/Portfolio-Website",
-  },
-  {
-    image: portfolioScreenShot,
-    heading: " Portfolio Website",
-    description: "Build the frountEnd Website using the React Framework ",
-    giticon: <BsGithub></BsGithub>,
-    liveicon: <RiLiveFill></RiLiveFill>,
-    live: "https://nikhil-sharma-portfolio.netlify.app/",
-    link: "https://github.com/sharmanikhil23/Portfolio-Website",
-  },
-  {
-    image: portfolioScreenShot,
-    heading: " Portfolio Website",
-    description: "Build the frountEnd Website using the React Framework ",
-    giticon: <BsGithub></BsGithub>,
-    liveicon: <RiLiveFill></RiLiveFill>,
-    live: "https://nikhil-sharma-portfolio.netlify.app/",
-    link: "https://github.com/sharmanikhil23/Portfolio-Website",
+    live: "https://to-do-list-by-nikhil.netlify.app",
+    link: "https://github.com/sharmanikhil23/To-Do-FrountEnd",
   },
 ];
 
