3e046ca4f6f66c75487d5172ba719022a2f20fc7
Nikhil Sharma <nikhil23062000@icloud.com>
2023-01-21 22:54:18 -0500

    Little change

--- src/components/complete/complete.js
+++ src/components/complete/complete.js
@@ -7,6 +7,31 @@ import { Projects } from "../projects/projects";
 import { Loading } from "./loading";
 
 const Completed = () => {
+  const [playAnimation, setPlayAnimation] = useState(true);
+  useEffect(() => {
+    const onPageLoad = () => {
+      setPlayAnimation(false);
+    };
+
+    // Check if the page has already loaded
+    if (document.readyState === "complete") {
+      onPageLoad();
+    } else {
+      window.addEventListener("load", onPageLoad);
+      // Remove the event listener when component unmounts
+      return () => window.removeEventListener("load", onPageLoad);
+    }
+  }, []);
+  return playAnimation === true ? (
+    <Loading></Loading>
+  ) : (
+    <>
+      <FirstPage></FirstPage>
+      <Projects></Projects>
+      <Footer></Footer>
+    </>
+  );
+
   //const [playAnimation, setPlayAnimation] = useState(true);
   // const temp = (
   //   <div
@@ -23,13 +48,13 @@ const Completed = () => {
   // if (playAnimation) {
   //   return <Loading></Loading>;
   // }
-  return (
-    <>
-      <FirstPage></FirstPage>
-      <Projects></Projects>
-      <Footer></Footer>
-    </>
-  );
+  // return (
+  // <>
+  //   <FirstPage></FirstPage>
+  //   <Projects></Projects>
+  //   <Footer></Footer>
+  // </>
+  // );
 };
 
 export const Complete = () => {
